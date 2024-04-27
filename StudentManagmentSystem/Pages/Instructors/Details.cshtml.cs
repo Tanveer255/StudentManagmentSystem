a7a04cdf9bc9bf333e0d135ab1b5bb4d2147b4e5
Muhammad Tanveer <tanveerofficial244@gmail.com>
2024-04-27 21:33:58 +0500

    Add project files.

---
+++ StudentManagmentSystem/Pages/Instructors/Details.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using StudentManagmentSystem.Data;
+using StudentManagmentSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace StudentManagmentSystem.Pages.Instructors;
+
+public class DetailsModel : PageModel
+{
+    private readonly SchoolContext _context;
+
+    public DetailsModel(SchoolContext context)
+    {
+        _context = context;
+    }
+
+    public Instructor Instructor { get; set; }
+
+    public async Task<IActionResult> OnGetAsync(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        Instructor = await _context.Instructors.FirstOrDefaultAsync(m => m.ID == id);
+
+        if (Instructor == null)
+        {
+            return NotFound();
+        }
+        return Page();
+    }
+}
